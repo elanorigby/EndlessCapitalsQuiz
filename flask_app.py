cca97aeb9f51551318f6a7aa82dfea243daac056
Sharlak <elanorigby@gmail.com>
2017-03-20 17:31:04 +0000

    started using sass and achieved a semblance of decency in how it looks

--- flask_app.py
+++ flask_app.py
@@ -1,36 +1,50 @@
-# needs
+from flask import Flask, render_template, request
 
-# title text
-# input field
-# choice buttons
-# display question area
-# display feedback area
-# stop please button
+from logic import CapitalsQuiz, CountriesQuiz, RandomQuiz
 
-from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    title = "Endless Capitals Quiz"
+    return render_template('index.html', title=title)
 
 
-@app.route("/capitals")
-def hello():
-    return "Hello World!"
+@app.route("/capitals", methods=['GET', 'POST'])
+def capitals():
+    title = "Endless Capitals Quiz"
+    quiz = CapitalsQuiz()
+    correct_ans = quiz.correct_answer
+    hint = quiz.hint
+    question = "What is the capital of {}?".format(hint)
 
+    #
+    # user_answer = request.form.user_answer.strip().lower()
+    # if quiz.is_right(user_answer):
+    #     feedback = "Yes! {} is the capital of {}".format(correct_ans, hint)
+    # else:
+    #     feedback = "Nope. Try Again."
 
-@app.route("/countries")
-def hello():
-    return "Hello World!"
+    # tell = request.form.tell
+    # if tell:
 
+    # correct_ans=correct_ans, hint=hint, feedback=feedback,
 
-@app.route("/random")
-def hello():
-    return "Hello World!"
+    return render_template('quiz.html', title=title, question=question, correct_ans=correct_ans, hint=hint)
 
-
+#
+#
+# @app.route("/countries")
+# def countries():
+#     return render_template('countries.html')  # pass variables
+#
+#
+# @app.route("/random")
+# def random():
+#     return render_template('random.html')  # pass variables
+#
+#
 if __name__ == "__main__":
     app.run()

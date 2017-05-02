4f0902bb02fba0d707a85c26b903d84de659eefb
Sharlak <elanorigby@gmail.com>
2017-05-02 18:51:09 +0100

    now passing a kind variable to data class to get correct data set

--- data.py
+++ data.py
@@ -6,29 +6,28 @@ class Data:
 
     db = TinyDB('db.json')
 
-    def grow_pair(self):
+    def __init__(self):
         """ Pick a random capital and country pair from db """
         pair_dict = choice(self.db.all())
-        country = pair_dict.get('country')
-        capital = pair_dict.get('capital')
-        return country, capital
+        self.country = pair_dict.get('country')
+        self.capital = pair_dict.get('capital')
 
-    def countries(self):
-        """ Assign the country to correct_answer and the capital to hint """
-        correct_answer, hint = self.grow_pair()
-        return correct_answer, hint
-
+    @property
     def capitals(self):
-        """ Assign the country to hint and the capital to correct_answer """
-        hint, correct_answer = self.grow_pair()
-        return correct_answer, hint
+        ans = self.capital
+        hint = self.country
+        return ans, hint
 
+    @property
+    def countries(self):
+        ans = self.country
+        hint = self.capital
+        return ans, hint
 
-
-
-# get a country and a capital from the db
-# sort based on quiz type
-# return them
-
-
-
+    def gimmie(self, kind):
+        if kind == 'capitals':
+            return self.capitals
+        elif kind == 'countries':
+            return self.countries
+        else:
+            ValueError("Not a valid kind of quiz.")

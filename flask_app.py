96fba4e11620f735bfa38101ddfdbbbe0a04554a
Sharlak <elanorigby@gmail.com>
2017-05-03 11:33:25 +0100

    new function for instantiating Quiz and assigning variables

--- flask_app.py
+++ flask_app.py
@@ -1,66 +1,47 @@
 import randomcolor
 from flask import Flask, render_template
-from random import choice
-from logic import CapitalsQuiz, CountriesQuiz
+from logic import Quiz
 
 def get_color(lum):
     rand_color = randomcolor.RandomColor()
-    color = str(rand_color.generate(luminosity=lum)[0])
-    return color
+    return str(rand_color.generate(luminosity=lum)[0])
+
+
+def make_quiz(kind):
+    quiz = Quiz(kind)
+    title = quiz.kind.capitalize()
+    question = quiz.question
+    ans = quiz.ans
+    hint = quiz.hint
+    bkcolor = get_color('light')
+
+    return title, question, ans, hint, bkcolor
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    title = "Capitals"
+    title = "Quiz"
     return render_template('index.html', title=title)
 
 
 @app.route("/capitals", methods=["GET", "POST"])
 def capitals():
-    quiz = CapitalsQuiz()
-    bkcolor = get_color('light')
-    title = quiz.name
-    correct_answer = quiz.correct_answer
-    hint = quiz.hint
-    question = "What is the capital of {}?".format(hint)
-
-    return render_template('quiz.html', title=title, question=question, correct_answer=correct_answer, hint=hint,
-                           bkcolor=bkcolor)
+    title, question, ans, hint, bkcolor = make_quiz('capitals')
+    return render_template('quiz.html', title=title, question=question, ans=ans, hint=hint, bkcolor=bkcolor)
 
 
 @app.route("/countries", methods=["GET", "POST"])
 def countries():
-    quiz = CountriesQuiz()
-    bkcolor = get_color('light')
-    title = quiz.name
-    correct_answer = quiz.correct_answer
-    hint = quiz.hint
-    question = "{} is the capital of what country?".format(hint)
-
-    return render_template('quiz.html', title=title, question=question, correct_answer=correct_answer, hint=hint,
-                           bkcolor=bkcolor)
+    title, question, ans, hint, bkcolor = make_quiz('countries')
+    return render_template('quiz.html', title=title, question=question, ans=ans, hint=hint, bkcolor=bkcolor)
 
 
 @app.route("/random", methods=["GET", "POST"])
 def random():
-    random_quiz = choice([CapitalsQuiz, CountriesQuiz])
-    quiz = random_quiz()
-    bkcolor = get_color('light')
-    title = quiz.name
-    correct_answer = quiz.correct_answer
-    hint = quiz.hint
-
-    if random_quiz == CapitalsQuiz:
-        question = "What is the capital of {}?".format(hint)
-    elif random_quiz == CountriesQuiz:
-        question = "{} is the capital of what country?".format(hint)
-    else:
-        raise TypeError
-
-    return render_template('quiz.html', title=title, question=question, correct_answer=correct_answer, hint=hint,
-                           bkcolor=bkcolor)
-
+    title, question, ans, hint, bkcolor = make_quiz('random')
+    return render_template('quiz.html', title=title, question=question, ans=ans, hint=hint, bkcolor=bkcolor)
 
 if __name__ == "__main__":
     app.run()

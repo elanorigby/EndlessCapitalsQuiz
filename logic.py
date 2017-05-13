38fc51a17f6311f6c204a8b89a63d751f56b8627
Sharlak <elanorigby@gmail.com>
2017-05-13 16:20:15 +0100

    commented

--- logic.py
+++ logic.py
@@ -2,6 +2,19 @@ from random import choice
 from data import Data
 
 class Quiz:
+    """
+     Pass in 'capitals', 'country', or 'random' (required)
+     when you create an new instance.
+     This will give you the properties
+     - instance.kind
+     - instance.ans
+     - instance.hint
+     - instance.question
+     and the method
+     - instance.is_correct(guess) -> returns True or False
+     to use in your project.
+     """
+
     def __init__(self, kind):
         if kind not in 'capitals countries random':
             raise ValueError("Not a valid quiz type")

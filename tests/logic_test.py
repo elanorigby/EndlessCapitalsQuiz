a77b1510ee35b062274a44327cb579a171a7eb79
Sharlak <elanorigby@gmail.com>
2017-07-07 17:02:51 +0100

    first tests written

--- tests/logic_test.py
+++ tests/logic_test.py
@@ -1,5 +1,24 @@
 from ccquiz.logic import Quiz
+import pytest
 
-# is_correct
-# assert self.ans -> true
+# capitals ---------
+# instantiate
+@pytest.fixture
+def caps():
+    return Quiz('capitals')
+
+# assert quiz.ans -> true
+def test_ans_gets_true(caps):
+    """" pass answer as guess."""
+    assert caps.is_correct(caps.ans) == True
+
 # assert somebullshit -> false
+def test_wrong_gets_false(caps):
+    """" pass random bs as guess"""
+    assert caps.is_correct('Townsville') == False
+
+def test_country_gets_false(caps):
+    """" pass hint as guess"""
+    assert caps.is_correct(caps.hint) == False
+
+
